0d09c55f1e13e4d0fbd206d22595d43b85789bff
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-07-20 20:10:40 +0100

    import and use Octokit to explore github api. First http request

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 import { config } from "./config.js";
+import { Octokit } from "https://esm.sh/octokit";
 
 const personalToken = config.personalToken;
+
+// Pass personal token to Octokit instance
+const octokit = new Octokit({
+  auth: personalToken,
+});
+
+try {
+  const response = await octokit.request("GET /users/benante", {
+    username: "USERNAME",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+  const userdata = response.data;
+  console.log(userdata);
+} catch (error) {
+  console.error("Error: ", error.message);
+}
